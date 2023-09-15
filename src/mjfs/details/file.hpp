edcd2ef00d03dd6afc4f00efb45cb83b97bab944
MateuszJanduraUszu <Mati4321@interia.pl>
2023-09-15 14:34:28 +0200

    Add `<file.hpp>`, `<file.cpp>` and `<details/file.hpp>`

---
+++ src/mjfs/details/file.hpp
@@ -0,0 +1,114 @@
+// file.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _MJFS_DETAILS_FILE_HPP_
+#define _MJFS_DETAILS_FILE_HPP_
+#include <cstdint>
+#include <mjfs/bitmask.hpp>
+#include <mjfs/file.hpp>
+#include <mjfs/tinywin.hpp>
+#include <type_traits>
+
+namespace mjfs {
+    namespace details {
+        class _Close_handle_guard {
+        public:
+            void* _Handle;
+
+            ~_Close_handle_guard() noexcept {
+                if (_Holds_valid_handle()) {
+                    ::CloseHandle(_Handle);
+                    _Handle = nullptr;
+                }
+            }
+
+            bool _Holds_valid_handle() const noexcept {
+                return _Handle != nullptr && _Handle != INVALID_HANDLE_VALUE;
+            }
+
+            void* _Release() noexcept { // release the stored handle
+                void* const _Result = _Handle;
+                _Handle             = nullptr;
+                return _Result;
+            }
+        };
+
+        [[nodiscard]] inline void* _Create_file(const wchar_t* const _Path, const file_share _Share,
+            const file_attribute _Attributes, const file_flag _Flags) noexcept {
+            return ::CreateFileW(_Path, GENERIC_ALL, static_cast<unsigned long>(_Share),
+                nullptr, CREATE_NEW, static_cast<unsigned long>(_Attributes)
+                    | static_cast<unsigned long>(_Flags), nullptr);
+        }
+
+        [[nodiscard]] inline void* _Open_file(const wchar_t* const _Target, const file_access _Access,
+            const file_share _Share, const file_flag _Flags) noexcept {
+            return ::CreateFileW(_Target, static_cast<unsigned long>(_Access),
+                static_cast<unsigned long>(_Share), nullptr, OPEN_EXISTING,
+                    static_cast<unsigned long>(_Flags) | FILE_ATTRIBUTE_NORMAL, nullptr);
+        }
+
+        inline file_attribute _Adjust_attributes(
+            const file_attribute _Attributes, const file_perms _Perms) noexcept {
+            if (_Perms == file_perms::readonly) {
+                return _Attributes | file_attribute::readonly;
+            } else {
+                return _Attributes & ~file_attribute::readonly;
+            }
+        }
+
+        inline FILE_BASIC_INFO _Get_file_basic_info(void* const _Handle) noexcept {
+            FILE_BASIC_INFO _Info = {0};
+            _Info.FileAttributes  = INVALID_FILE_ATTRIBUTES;
+            return ::GetFileInformationByHandleEx(_Handle, FileBasicInfo, ::std::addressof(_Info),
+                sizeof(FILE_BASIC_INFO)) != 0 ? _Info : FILE_BASIC_INFO{};
+        }
+
+        inline file_attribute _Get_file_attributes(void* const _Handle) noexcept {
+            return static_cast<file_attribute>(_Get_file_basic_info(_Handle).FileAttributes);
+        }
+
+        inline bool _Is_file_readonly(const file_attribute _Attributes) noexcept {
+            return _Has_bits(_Attributes, file_attribute::readonly);
+        }
+        
+        template <FILE_INFO_BY_HANDLE_CLASS _Class, class _Ty>
+        inline bool _Set_file_information(void* const _Handle, _Ty& _Info) noexcept {
+            return ::SetFileInformationByHandle(
+                _Handle, _Class, ::std::addressof(_Info), sizeof(_Ty)) != 0;
+        }
+
+        inline bool _Set_file_attributes(
+            void* const _Handle, const file_attribute _New_attributes) noexcept {
+            FILE_BASIC_INFO _Info = _Get_file_basic_info(_Handle);
+            _Info.FileAttributes  = static_cast<unsigned long>(_New_attributes);
+            return _Set_file_information<FileBasicInfo>(_Handle, _Info);
+        }
+
+        inline uint64_t _Get_file_size(void* const _Handle) noexcept {
+            unsigned long _High = 0;
+            return static_cast<uint64_t>(
+                ::GetFileSize(_Handle, &_High)) | (static_cast<uint64_t>(_High) << 32);
+        }
+
+        inline bool _Seek_file(void* const _Handle, const uint64_t _New_pos) noexcept {
+            const long _Low = static_cast<long>(_New_pos & 0x0000'0000'FFFF'FFFF);
+            long _High      = static_cast<long>((_New_pos & 0xFFFF'FFFF'0000'0000) >> 32);
+            return ::SetFilePointer(_Handle, _Low, &_High, FILE_BEGIN) != INVALID_SET_FILE_POINTER;
+        }
+
+        inline uint64_t _Resize_file(void* const _Handle, const uint64_t _New_size) noexcept {
+            return _Seek_file(_Handle, _New_size) && ::SetEndOfFile(_Handle) != 0;
+        }
+
+        inline bool _Set_file_delete_flag(void* const _Handle) noexcept {
+            FILE_DISPOSITION_INFO _Info;
+            _Info.DeleteFileW = true;
+            return _Set_file_information<FileDispositionInfo>(_Handle, _Info);
+        }
+    } // namespace details
+} // namespace mjfs
+
+#endif // _MJFS_DETAILS_FILE_HPP_
---
+++ src/mjfs/file.cpp
@@ -0,0 +1,156 @@
+// file.cpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <mjfs/details/file.hpp>
+#include <mjfs/details/status.hpp>
+#include <mjfs/file.hpp>
+#include <type_traits>
+
+namespace mjfs {
+    file::file() noexcept : _Myhandle(_Invalid_handle) {}
+
+    file::file(file&& _Other) noexcept : _Myhandle(_Other._Myhandle) {
+        _Other._Myhandle = _Invalid_handle;
+    }
+
+    file::file(const path& _Target, const file_access _Access, const file_share _Share,
+        const file_flag _Flags) : _Myhandle(_Invalid_handle) {
+        (void) open(_Target, _Access, _Share, _Flags);
+    }
+
+    file::~file() noexcept {
+        close();
+    }
+
+    file& file::operator=(file&& _Other) noexcept {
+        if (this != ::std::addressof(_Other)) {
+            _Myhandle        = _Other._Myhandle;
+            _Other._Myhandle = _Invalid_handle;
+        }
+
+        return *this;
+    }
+
+    bool file::is_open() const noexcept {
+        return _Myhandle != nullptr && _Myhandle != _Invalid_handle;
+    }
+
+    const file::native_handle_type file::native_handle() const noexcept {
+        return _Myhandle;
+    }
+
+    void file::close() noexcept {
+        if (is_open()) {
+            ::CloseHandle(_Myhandle);
+            _Myhandle = _Invalid_handle;
+        }
+    }
+
+    bool file::open(
+        const path& _Target, const file_access _Access, const file_share _Share, const file_flag _Flags) {
+        if (is_open()) { // some handle is currently held
+            return false;
+        }
+
+        _Myhandle = details::_Open_file(_Target.c_str(), _Access, _Share, _Flags);
+        return is_open();
+    }
+
+    bool file::set_handle(const native_handle_type _New_handle) noexcept {
+        if (is_open()) { // some handle is currently held
+            return false;
+        }
+
+        _Myhandle = _New_handle;
+        return true;
+    }
+
+    uint64_t file::size() const noexcept {
+        return is_open() ? details::_Get_file_size(_Myhandle) : 0;
+    }
+
+    bool file::resize(const uint64_t _New_size) noexcept {
+        return is_open() ? details::_Resize_file(_Myhandle, _New_size) : false;
+    }
+
+    file_attribute file::attributes() const noexcept {
+        return is_open() ? details::_Get_file_attributes(_Myhandle) : file_attribute::unknown;
+    }
+
+    bool file::attributes(const file_attribute _New_attributes) noexcept {
+        return is_open() ? details::_Set_file_attributes(_Myhandle, _New_attributes) : false;
+    }
+
+    file_perms file::permissions() const noexcept {
+        if (!is_open()) {
+            return file_perms::unknown;
+        }
+
+        return details::_Is_file_readonly(details::_Get_file_attributes(_Myhandle))
+            ? file_perms::readonly : file_perms::all;
+    }
+
+    bool file::permissions(const file_perms _New_perms) noexcept {
+        if (!is_open()) {
+            return false;
+        }
+
+        file_attribute _Attributes = details::_Get_file_attributes(_Myhandle);
+        if (_New_perms == file_perms::readonly) {
+            _Attributes |= file_attribute::readonly;
+        } else {
+            _Attributes &= ~file_attribute::readonly;
+        }
+
+        return details::_Set_file_attributes(_Myhandle, _Attributes);
+    }
+
+    bool create_file(const path& _Path, const file_share _Share, const file_attribute _Attributes,
+        const file_flag _Flags, const file_perms _Perms, file* const _File) {
+        const bool _Store_handle                  = _File != nullptr && !_File->is_open();
+        const file_attribute _Adjusted_attributes = details::_Adjust_attributes(_Attributes, _Perms);
+        details::_Close_handle_guard _Guard       = {details::_Create_file(
+            _Path.c_str(), _Share, _Adjusted_attributes, _Flags)};
+        if (!_Guard._Holds_valid_handle()) {
+            return false;
+        }
+
+        if (!_Store_handle) {
+            return true;
+        }
+
+        // Note: We can call file::set_handle() without any checks, because it only fails
+        //       when any handle is currently held. We already checked it via _Store_handle.
+        (void) _File->set_handle(_Guard._Release());
+        return true;
+    }
+
+    bool create_file(const path& _Path, file* const _File) {
+        return ::mjfs::create_file(_Path, file_share::none, file_attribute::normal,
+            file_flag::none, file_perms::all, _File);
+    }
+
+    bool delete_file(const path& _Target) {
+        return ::DeleteFileW(_Target.c_str()) != 0;
+    }
+
+    bool delete_file(file& _File) noexcept {
+        if (!_File.is_open()) {
+            return false;
+        }
+
+        if (details::_Set_file_delete_flag(_File.native_handle())) {
+            _File.close(); // automatically deletes on close
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool rename(const path& _Old_path, const path& _New_path) {
+        return ::MoveFileExW(_Old_path.c_str(), _New_path.c_str(),
+            MOVEFILE_COPY_ALLOWED | MOVEFILE_REPLACE_EXISTING) != 0;
+    }
+} // namespace mjfs
