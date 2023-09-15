80b3e50a91116fe49271c6789c4cab364d3aff55
MateuszJanduraUszu <Mati4321@interia.pl>
2023-09-15 14:32:12 +0200

    Add `<tinywin.hpp>`

---
+++ src/mjfs/tinywin.hpp
@@ -0,0 +1,13 @@
+// tinywin.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _MJFS_TINYWIN_HPP_
+#define _MJFS_TINYWIN_HPP_
+
+#define WIN32_LEAN_AND_MEAN
+#define NOSERVICE
+#include <Windows.h>
+#endif // _MJFS_TINYWIN_HPP_
